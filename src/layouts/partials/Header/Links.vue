4b5742db98c3b960744afb5cd848fe63549776e5
Nev <nevilain@gmail.com>
2020-05-01 03:03:13 +0200

    Animation GDA

--- src/layouts/partials/Header/Links.vue
+++ src/layouts/partials/Header/Links.vue
@@ -11,7 +11,10 @@
         <g-link to="/contribuer/" class="font-semibold text-font border-none">
             Contribuer
         </g-link>
-        <a href="https://gamedevalliance.fr" class="font-semibold text-marvin border-none">GameDevAlliance</a>
+        <a href="https://gamedevalliance.fr" class="font-semibold text-marvin border-none gda">
+            <span>G</span><span>a</span><span>m</span><span>e</span><span>D</span><span>e</span><span>v</span>
+            <span>A</span><span>l</span><span>l</span><span>i</span><span>a</span><span>n</span><span>c</span><span>e</span>
+        </a>
         <a href="https://discord.gg/RrBppaj" class="font-semibold text-purple-600 border-none">Rejoindre Discord</a>
     </nav>
 </template>
@@ -28,6 +31,30 @@
             }
         }
     }
+    .gda span {
+        @apply inline-block;
+    }
+    .gda:hover span { animation:bounce .45s; }
+    @keyframes bounce {
+      0%,100%{ transform:translate(0); }
+      20%{ transform:rotateX(20deg) translateY(1px) rotate(-3deg); }
+      40%{ transform:translateY(-5px) rotate(3deg) scale(1.1);  }
+    }
+
+    .gda span:nth-child(2){ animation-delay:.05s; }
+    .gda span:nth-child(3){ animation-delay:.1s; }
+    .gda span:nth-child(4){ animation-delay:.15s; }
+    .gda span:nth-child(5){ animation-delay:.2s; }
+    .gda span:nth-child(6){ animation-delay:.25s; }
+    .gda span:nth-child(7){ animation-delay:.3s; }
+    .gda span:nth-child(8){ animation-delay:.35s; }
+    .gda span:nth-child(9){ animation-delay:.4s; }
+    .gda span:nth-child(10){ animation-delay:.45s; }
+    .gda span:nth-child(11){ animation-delay:.5s; }
+    .gda span:nth-child(12){ animation-delay:.55s; }
+    .gda span:nth-child(13){ animation-delay:.6s; }
+    .gda span:nth-child(14){ animation-delay:.65s; }
+    .gda span:nth-child(15){ animation-delay:.7s; }
 </style>
 
 <static-query>
