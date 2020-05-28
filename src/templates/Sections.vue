70b24c105c01de7a54a1357523bb2530b2b63e9b
Princesseuh <princssdev@gmail.com>
2020-05-28 09:21:08 -0400

    Fix section and course style not applying to the right element

--- src/templates/Sections.vue
+++ src/templates/Sections.vue
@@ -46,125 +46,123 @@
 </script>
 
 <style lang="postcss" scoped>
-    article >>> {
-        & main {
-            /*
+    section >>> {
+        /*
                 NOTE: This style is used for captions of images and videos. Very practical for the authors
                 as they only have to do a line return and write in italics. - nev, 2020-04-24
             */
-            & img + noscript + em {
-                @apply text-center text-sm text-font-2 mt-6 mb-0 block;
+        & img + noscript + em {
+            @apply text-center text-sm text-font-2 mt-6 mb-0 block;
+        }
+
+        & ul {
+            @apply list-disc list-inside;
+        }
+
+        & ol {
+            @apply list-decimal list-inside;
+        }
+
+        & li {
+            @apply my-2;
+        }
+
+        & pre, & code {
+            @apply border-0 bg-area text-sm mb-6 mt-3 rounded-md font-display;
+
+            text-shadow: none;
+            box-shadow: none;
+        }
+
+        & code {
+            @apply px-2 py-1;
+        }
+
+        & blockquote {
+            @apply border-solid border-l-4 border-astride;
+
+            & p {
+                @apply m-5 italic;
+            }
+        }
+
+        & .bubble {
+            & .bubble-content {
+                @apply inline-block text-left bg-area py-2 px-8 my-4 rounded-full;
+
+                margin-left: -20px;
+                margin-right: -20px;
+                max-width: 89%;
             }
 
-            & ul {
-                @apply list-disc list-inside;
+            & h5 {
+                @apply inline-block text-right m-0 py-1 px-5 relative text-lg bg-area-2 rounded-lg;
+
+                top: 2rem;
+                margin-top: -2rem;
+            }
+        }
+
+        & .bubble-marvin {
+            @apply text-right;
+
+            & h5 {
+                @apply text-marvin;
+
+                right: 116px;
             }
 
-            & ol {
-                @apply list-decimal list-inside;
+            &::after {
+                background-image: url("~@avatar/marvin.png");
+                content: "";
+                float: right;
+                width: 100px;
+                height: 100px;
+                bottom: 0;
+                position: relative;
+                display: inline-block;
+            }
+        }
+
+        & .bubble-astride {
+            @apply text-left;
+
+            & h5 {
+                @apply text-astride;
+
+                left: 116px;
             }
 
-            & li {
-                @apply my-2;
+            &::after {
+                background-image: url("~@avatar/astride.png");
+                content: "";
+                float: left;
+                width: 100px;
+                height: 100px;
+                bottom: 0;
+                position: relative;
+                display: inline-block;
+            }
+        }
+
+        & .bubble-remi {
+            @apply text-left;
+
+            & h5 {
+                @apply text-remi;
+
+                left: 116px;
             }
 
-            & pre, & code {
-                @apply border-0 bg-area text-sm mb-6 mt-3 rounded-md font-display;
-
-                text-shadow: none;
-                box-shadow: none;
-            }
-
-            & code {
-                @apply px-2 py-1;
-            }
-
-            & blockquote {
-                @apply border-solid border-l-4 border-astride;
-
-                & p {
-                    @apply m-5 italic;
-                }
-            }
-
-            & .bubble {
-                & .bubble-content {
-                    @apply inline-block text-left bg-area py-2 px-8 my-4 rounded-full;
-
-                    margin-left: -20px;
-                    margin-right: -20px;
-                    max-width: 89%;
-                }
-
-                & h5 {
-                    @apply inline-block text-right m-0 py-1 px-5 relative text-lg bg-area-2 rounded-lg;
-
-                    top: 2rem;
-                    margin-top: -2rem;
-                }
-            }
-
-            & .bubble-marvin {
-                @apply text-right;
-
-                & h5 {
-                    @apply text-marvin;
-
-                    right: 116px;
-                }
-
-                &::after {
-                    background-image: url("~@avatar/marvin.png");
-                    content: "";
-                    float: right;
-                    width: 100px;
-                    height: 100px;
-                    bottom: 0;
-                    position: relative;
-                    display: inline-block;
-                }
-            }
-
-            & .bubble-astride {
-                @apply text-left;
-
-                & h5 {
-                    @apply text-astride;
-
-                    left: 116px;
-                }
-
-                &::after {
-                    background-image: url("~@avatar/astride.png");
-                    content: "";
-                    float: left;
-                    width: 100px;
-                    height: 100px;
-                    bottom: 0;
-                    position: relative;
-                    display: inline-block;
-                }
-            }
-
-            & .bubble-remi {
-                @apply text-left;
-
-                & h5 {
-                    @apply text-remi;
-
-                    left: 116px;
-                }
-
-                &::after {
-                    background-image: url("~@avatar/remi.png");
-                    content: "";
-                    float: left;
-                    width: 100px;
-                    height: 100px;
-                    bottom: 0;
-                    position: relative;
-                    display: inline-block;
-                }
+            &::after {
+                background-image: url("~@avatar/remi.png");
+                content: "";
+                float: left;
+                width: 100px;
+                height: 100px;
+                bottom: 0;
+                position: relative;
+                display: inline-block;
             }
         }
     }
