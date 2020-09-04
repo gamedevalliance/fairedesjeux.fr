f560e495fb7c401e4b7f8069e195a7f190841436
Princesseuh <princssdev@gmail.com>
2020-09-04 15:07:00 -0400

    Menu mobile

--- src/layouts/partials/Header/MobileMenu.vue
+++ src/layouts/partials/Header/MobileMenu.vue
@@ -3,14 +3,41 @@
         :class="['mobile--menu', state ? 'open' : '',]"
     >
         <div class="p-4">
-            <g-link v-for="menuCourse in $static.menuCourses.edges"
-                    :key="menuCourse.node.header"
-                    :to="menuCourse.node.path"
-                    class="link text-font"
-                    :title="menuCourse.node.title"
-            >
-                {{ menuCourse.node.short_title || menuCourse.node.title }}
-            </g-link>
+            <div v-if="!$page.section">
+                <g-link v-for="menuCourse in $static.menuCourses.edges"
+                        :key="menuCourse.node.header"
+                        :to="menuCourse.node.path"
+                        class="link text-font"
+                        :title="menuCourse.node.title"
+                >
+                    {{ menuCourse.node.short_title || menuCourse.node.title }}
+                </g-link>
+            </div>
+
+            <div v-if="$page.section.chapter" class="mb-3">
+                <g-link :to="$page.section.chapter.course.path" class="link text-font">
+                    « Retour vers {{ $page.section.chapter.course.short_title || $page.section.chapter.course.title }}
+                </g-link>
+            </div>
+
+            <div v-if="$page.section">
+                <div v-for="chapter in $page.section.chapters || $page.section.chapter.course.chapters"
+                     :key="chapter.id"
+                     class="mb-4"
+                >
+                    <span class="text-font text-xl mb-1 block">{{ chapter.title }}</span>
+                    <g-link v-for="section in chapter.sections"
+                            :key="section.id"
+                            :to="section.path"
+                            class="link text-font whitespace-normal"
+                            :title="section.title"
+                    >
+                        {{ section.title }}
+                    </g-link>
+                </div>
+            </div>
+
+            <hr class="my-3 border-body border-2 rounded-md border-opacity-75" />
 
             <g-link to="/contribuer/" class="link text-font mt-2">
                 Contribuer
@@ -21,8 +48,6 @@
             </a>
 
             <a href="https://discord.gg/RrBppaj" class="link text-purple-600">Rejoindre Discord</a>
-
-            <!-- <hr class="my-3 border-body border-2 rounded-md border-opacity-75" /> -->
         </div>
     </nav>
 </template>
