3c22954fa858b1185493df55bb3de13fe5e20ce6
Goulven Nev <nevilain@gmail.com>
2021-09-25 00:43:51 +0200

    Add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import Index from "./pages/Index.vue";
 import "tailwindcss/tailwind.css";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-createApp(App).mount("#app");
+const Home = { template: "<Index />" };
+
+const routes = [{ path: "/", component: Home }];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(Index).use(router).mount("#app");
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="mx-auto my-20 max-w-screen-sm text-center">
-    <img alt="Vue logo" class="h-40 mx-auto" src="./assets/logo.png" />
+    <img alt="Vue logo" class="h-40 mx-auto" src="../assets/logo.png" />
     <HelloWorld />
   </main>
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from "./components/HelloWorld.vue";
+  import HelloWorld from "../components/HelloWorld.vue";
 </script>
