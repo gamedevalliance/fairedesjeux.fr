596db3bd52a9e357e214e6676df2b593a819370f
Princesseuh <princssdev@gmail.com>
2022-12-13 19:06:02 -0400

    feat(content): Setup autoimports of components commonly used in content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
 import mdx from "@astrojs/mdx"
 import tailwind from "@astrojs/tailwind"
+import AutoImport from "astro-auto-import"
 import { defineConfig } from "astro/config"
 import renpyLang from "./scripts/renpyLang.mjs"
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://fairedesjeux.fr",
-	integrations: [tailwind(), mdx()],
+	integrations: [
+		tailwind(),
+		AutoImport({
+			imports: [
+				{
+					"./src/components/content/Bulle.astro": [["default", "Bulle"]],
+					"./src/components/content/Callout.astro": [["default", "Callout"]],
+					"./src/components/content/Video.astro": [["default", "Video"]],
+				},
+			],
+		}),
+		mdx(),
+	],
 	markdown: {
 		shikiConfig: {
 			langs: [renpyLang],
